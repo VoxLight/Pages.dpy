c54beae1a1794a6ade014884c593f949f03e45f0
56curious <george.potts153@gmail.com>
2021-04-16 11:12:38 +0100

    Update pages.py

--- pages.py
+++ pages.py
@@ -1,4 +1,3 @@
-# GUI Pages for Discord.py
 import time
 import asyncio
 import random
@@ -28,18 +27,16 @@ def _add_fields(embed, fields):
 def add_fields_to_embeds(template, field_pages):
     template.clear_fields()
     # [[f1, f2, f3], [f4, f5, f6], [f7, f8, f9], ....etc]
-    
+
     embeds = []
     for fields in field_pages:
         e = template.copy()
-        
+
         add_fields_to_embed(e, fields)
 
-            
         embeds.append(e)
 
     return embeds
-    
 
 
 def results_per_page(items, maxResults):
@@ -47,55 +44,52 @@ def results_per_page(items, maxResults):
     neededPages = math.ceil(len(items)/maxResults)
     for i in range(neededPages):
         results = items[i*maxResults:(i+1)*maxResults]
-        pagedResults.append(results)   
+        pagedResults.append(results)
 
     return pagedResults
 
 
-
-def _add_reacts(msg, rs):
+async def _add_reacts(msg, rs):
     for r in rs:
-        msg.add_reaction(r)
-
-
+        await msg.add_reaction(r)
 
 
 async def paginate(
-            bot,
-            ctx, 
-            pages, 
-            target_user=None, 
-            duration=60000, 
-            remove_on_finish=True
-        ):
+    bot,
+    ctx,
+    pages,
+    target_user=None,
+    duration=60000,
+    remove_on_finish=True
+):
     # You can specify a target user (the user whose reacts we care about)
     # as we may not always want the author of an invoked command as the target.
     # i.e. [p]propose @user "@user, do you want to marry @author?" etc.
     # However, not specifying one just defaults to the ctx.author
     if target_user == None:
         target_user = ctx.author
-    
+
     # The reactions we want to handle for.
     rw, b, f, ff, x = sym = ("⏪", "⬅️", "➡️", "⏩", "❌")
-    
 
     # page markers
     first_page, last_page = 0, len(pages)-1
-    
+
     current_page = first_page
-    
+
     # The message that will display in chat and that the user will react to.
     book = await ctx.send(embed=pages[current_page])
-    
+
     # Add all of our reactions to the message
-    _add_reacts(book, sym)
-    
+    await _add_reacts(book, sym)
+
     # u (User who added the reaction) | r (The Reaction object itself)
-    
+
     # 1. check that the one who initially invoked the command is the one who reacted
     # 2. check that the message being reacted to is the confirmation message
     # 3. check that the reaction that was added is one we handle for.
-    check = lambda r, u: (u == target_user) and (r.msg.id == book.id) and (r.name in sym)
+    def check(r, u): return (u == target_user) and (
+        r.message.id == book.id) and (r.name in sym)
     while 1:
         try:
             r, u = await bot.wait_for('reaction_add', timeout=60.0, check=check)
@@ -108,18 +102,24 @@ async def paginate(
             return
         else:
             # Rewind
-            if r.emoji == rw: current_page = first_page  
+            if r.emoji == rw:
+                current_page = first_page
             # FastForward
-            elif r.emoji == ff: current_page = last_page  
+            elif r.emoji == ff:
+                current_page = last_page
             # Back
             elif r.emoji == b:
-                if current_page == 0: current_page = last_page
-                else: current_page -= 1   
+                if current_page == 0:
+                    current_page = last_page
+                else:
+                    current_page -= 1
             # forward
-            elif r.emoji == f:     
-                if current_page == last_page: current_page = first_page
-                else: current_page += 1
-                
+            elif r.emoji == f:
+                if current_page == last_page:
+                    current_page = first_page
+                else:
+                    current_page += 1
+
             book.edit(embed=pages[current_page])
 
 # Usage
@@ -130,16 +130,16 @@ async def paginate(
 # else: # they said no
 #     do other stuff
 
-async def confirm(
-            bot,
-            ctx,
-            page,
-            target_user=None,
-            duration=20.0, 
-            remove_on_finish=True
-        ):
-    """Prompts a user to confirm. React with a yes for true, react with an X for false.
 
+async def confirm(
+    bot,
+    ctx,
+    page,
+    target_user=None,
+    duration=20.0,
+    remove_on_finish=True
+):
+    """Prompts a user to confirm. React with a yes for true, react with an X for false.
     Args:
         ctx (discord.ext.commands.Context): Invocation context of a command.
         page (discord.Embed): The embed to display and have a user react to.
@@ -148,55 +148,50 @@ async def confirm(
         duration (float, optional): How long to wait before timing out. Defaults to 20.0.
         remove_on_finish (bool, optional): Whether or not the bot should remove the 
         message once reactions have finished. Defaults to True.
-
     Returns:
         [Bool]: If the user accepted or denied the confirmation.
     """
-    
+
     # You can specify a target user (the user whose reacts we care about)
     # as we may not always want the author of an invoked command as the target.
     # i.e. [p]propose @user "@user, do you want to marry @author?" etc.
     # However, not specifying one just defaults to the ctx.author
     if target_user == None:
         target_user = ctx.author
-    
+
     # The reactions we want to handle for.
     x, _ = sym = ("✅", "❌")
-    
-    
-    
+
     # The message that will display in chat and that the user will react to.
     confirmation_message = await ctx.send(embed=page)
-    
+
     # Add all of our reactions to the message
-    _add_reacts(confirmation_message, sym)
-    
+    await _add_reacts(confirmation_message, sym)
+
     # u (User who added the reaction) | r (The Reaction object itself)
-    
+
     # 1. check that the one who initially invoked the command is the one who reacted
     # 2. check that the message being reacted to is the confirmation message
     # 3. check that the reaction that was added is one we handle for.
-    check = lambda r, u: (u == target_user) and (r.msg.id == confirmation_message.id) and (r.name in sym)
-    
+    def check(r, u): return (u == target_user) and (
+        r.message.id == confirmation_message.id) and (r.emoji in sym)
+
     try:
         r, u = await bot.wait_for('reaction_add', timeout=duration, check=check)
-        
+
     except asyncio.TimeoutError:
         if remove_on_finish:
             await confirmation_message.delete()
         else:
             await confirmation_message.edit("*(This message is no longer active)*", embed=page)
         return False
-    
+
     else:
         if remove_on_finish:
             await confirmation_message.delete()
         else:
             await confirmation_message.edit("*(This message is no longer active)*", embed=page)
-            
+
         if r.emoji == x:
             return True
         return False
-            
-    
-    
